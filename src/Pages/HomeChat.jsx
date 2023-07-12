a525d0379c38c37534fd435164828e6142bce435
FergusonLyn <psky730@gmail.com>
2023-07-12 22:22:36 +0800

    additions

--- src/Pages/HomeChat.jsx
+++ src/Pages/HomeChat.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SideBar from '../chat-components/SideBar'
 import Chat from '../chat-components/Chat'
 import "../stylesheets/chatstyles.css"
+import "../stylesheets/styles.scss"
 
 const HomeChat = () => {
   return (
